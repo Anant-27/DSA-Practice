321d7c940078c93a40c8a548c34b5651905cc7c7
Anant-27 <anantseth2709@gmail.com>
2025-08-27 23:07:13 +0530

    Create Best Time to Buy and Sell Stock.java

---
+++ Leetcode/Best Time to Buy and Sell Stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        Scanner sc=new Scanner(System.in);
+        int min_price=Integer.MAX_VALUE;
+        int max_profit=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min_price){
+                min_price=prices[i];
+            }
+            int profit=prices[i]-min_price;
+            if(profit>max_profit){
+                max_profit=profit;
+            }
+        }
+            return max_profit;
+    }
+}
