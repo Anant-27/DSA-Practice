d38e8e4f7c2a9a00bc5c29c0ebd36a883129e14d
Anant-27 <anantseth2709@gmail.com>
2025-10-16 22:46:32 +0530

    Implement partitionArray method in Solution class

---
+++ Leetcode/Partition Array Such That Maximum Difference Is K.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int count=1;
+        int minVal=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]-minVal > k){
+                count++;
+                minVal=nums[i];
+            }
+        }
+        return count;
+    }
+}
